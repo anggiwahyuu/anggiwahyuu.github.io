e7f5c2190b9d18da284794fbd2844b953ee15577
anggiwahyuu <anggiwahyu543@gmail.com>
2022-03-08 09:27:55 +0700

    first commit

---
+++ precache-manifest.226e68de65be5316b69557d5134bbc25.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a48d51a27f8f180a3f26",
+    "url": "/css/app.902e34fe.css"
+  },
+  {
+    "revision": "6c7f4ae0eca2af9a52f3",
+    "url": "/css/contact.b9e341ab.css"
+  },
+  {
+    "revision": "fdad73b699a671ce28e9",
+    "url": "/css/profile.14cbd3f6.css"
+  },
+  {
+    "revision": "673bb87a614784e20ef2628873cd9e17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "852b2b3d1ab9de525e2c",
+    "url": "/js/about.824a85ac.js"
+  },
+  {
+    "revision": "a48d51a27f8f180a3f26",
+    "url": "/js/app.b4da8681.js"
+  },
+  {
+    "revision": "ecda6f5c34b643e2aff3",
+    "url": "/js/chunk-vendors.287cc30c.js"
+  },
+  {
+    "revision": "6c7f4ae0eca2af9a52f3",
+    "url": "/js/contact.93ac0783.js"
+  },
+  {
+    "revision": "fdad73b699a671ce28e9",
+    "url": "/js/profile.4fc1a415.js"
+  },
+  {
+    "revision": "ec42e1a3047f24c27d19",
+    "url": "/js/user.4498ae49.js"
+  },
+  {
+    "revision": "38c479585f3160952bf213e093146e27",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
